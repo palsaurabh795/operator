e16424e9651c9516b08023122233d59390374984
Saurabh Pal <pal.saurabhh@gmail.com>
2024-11-23 18:17:16 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,21 @@
+public class operator {
+    public static void main(String[] args){
+        int a= 10;
+        int b= 26-a;
+        System.out.println(b);
+        System.out.println(6==8);
+        System.out.println(64>5 && 64>98);
+    }
+}
+
+
+
+
+public class operator PRECEDENCE {
+    public static void main(String[] args){
+        int a = 6*5-34/2;
+        int b = 6/5-34*2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
